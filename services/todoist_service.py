c4e303350050b17ec2241ec144b4d9f92157dfec
zao <ayalaorez@gmail.com>
2025-03-30 22:24:55 -0500

    todoist service config manager added

--- services/todoist_service.py
+++ services/todoist_service.py
@@ -34,4 +34,17 @@ class TodoistService:
             return []
 
 
+if __name__ == '__main__':
+    import sys
 
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
+    if parent_dir not in sys.path:
+        sys.path.append(parent_dir)
+    
+    try:
+        from config import ConfigManager
+    except ImportError:
+        print("Error: Could not import ConfigManager. Make sure 'config.py' exists in the parent directory.")
+        sys.exit(1)
+    print("todoist config manager init correclty")
